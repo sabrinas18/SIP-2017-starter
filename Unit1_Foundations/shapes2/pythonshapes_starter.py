185ec0637830454c55c3b2497134af4c6c14d3c0
sabrinas18 <sabrinastone22@gmail.com>
2017-07-06 12:36:05 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -11,6 +11,16 @@ y_pos = -150
 t.setposition(x_pos, y_pos)
 
 ### Write your code below:
+fillcolor("purple")
+n = input('how many sides of a shape?: ')
+print(n)
+begin_fill()
+move = 500
+for move in range(n):
+    move = forward(180)
+    move = right(360/n)
+end_fill()
+
 
 
 
